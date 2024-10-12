c7d9ebb395acaf72a3cc5f78a6d0086c5ba67be5
zeng715734199 <715734199@qq.com>
2024-10-12 12:58:51 +0800

    Update: 抽屉样式调整

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { createHashRouter, RouterProvider } from 'react-router-dom'
 import routes from './config'
 const router = createHashRouter(routes)
